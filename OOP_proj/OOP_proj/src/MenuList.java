85621a8910afecca28f5666297aa83581e885280
junsy <junsy0901@gmail.com>
2024-05-06 00:03:07 +0900

    03_0506_add MenuList.java
    새 클래스 파일을 만들었습니다

---
+++ OOP_proj/OOP_proj/src/MenuList.java
@@ -0,0 +1,17 @@
+
+public class MenuList {
+	public int value;
+	public int wait_time;
+	
+	class Hamburger extends MenuList{
+		
+	}
+	
+	class Dessert extends MenuList{
+		
+	}
+	
+	class Beverage extends MenuList{
+		
+	}
+}
