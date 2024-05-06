d90f7cc245fb3ea5a41c32d5b68a9003914537e0
junsy <junsy0901@gmail.com>
2024-05-06 18:28:35 +0900

    06_0505_Change Main
    - OrderLoops의 일부 코드 최적화
    - Order_burger_set(String answer)의 일부 코드 최적화
    - ADD Order_burger_set() 버거 세트 구성품을 변경하는 함수 추가
    - 그 외 함수 순서 변경 및 주석 추가
    
    Issue
    - orderhistory에 새 내역을 append 할 수 없는 치명적 오류
    (불고기 세트 > 새우 단품 주문 시 새우는 리스트에 없음)
    - 주문 내역 출력 시 두 번 출력되는 오류

--- OOP_proj/OOP_proj/src/Main.java
+++ OOP_proj/OOP_proj/src/Main.java
@@ -9,7 +9,7 @@ public class Main {
 	static Scanner scanner = new Scanner(System.in);
 	
 	
-	// 주문 내역 저장을 위한 class 생성
+	// 주문 내역 저장을 위한 class
 	static class OrderHistory {
     	public String name;	// 메뉴 이름
         public int price; 	// 가격
@@ -32,11 +32,12 @@ public class Main {
 	    // 주문 내역을 출력하는 함수(임시, 두 번씩 출력되는 개같은 오류 있으니 추후 수정 요망)
 	    public static void printOrder() {
             for (OrderHistory item : orderhistory) {
-                System.out.println(item.name);
+                System.out.println(item);
             }
         }
     }
-		
+	
+	// main 시뮬레이션 함수
 	public static void Simulation() {
 		// 메인 시뮬레이션	
 		System.out.println("[system] 롯X리아 입장");
@@ -80,88 +81,50 @@ public class Main {
 		// Todo 대기 후 음식 픽업 구현
 	}
 	
+	// 반복 주문을 위한 함수 
 	public static void OrderLoops() {
 		// 여기에 메뉴 출력, 메뉴 선택 구현
 		String answer;
+		boolean loop_or_not = false;
 		
-		System.out.println("[키오스크] 메뉴를 선택해주세요.");
-		System.out.print(">>");
-		answer = scanner.nextLine();
-
-		if(answer.matches(".*메뉴.*")) {	// '메뉴' 단어만 포함되면 메뉴를 출력, (참고)정규표현식 사용
-			MenuList.printMenu();	// MenuList의 함수 사용, 필요 시 변경
-		}
-		else {
-			String buffer = answer;
-			while(true) {
-				if(answer.matches(".*세트.*")) {
-					Order_burger_set(buffer);
-					break;
-				}
-				else if(answer.matches(".*단품.*")) {
-					Order_burger_single(buffer);
-					break;
+		do {
+			System.out.println("[키오스크] 메뉴를 선택해주세요.");
+			System.out.print(">>");
+			answer = scanner.nextLine();
+			
+			if(answer.matches(".*메뉴.*")) {	// '메뉴' 단어만 포함되면 메뉴를 출력, (참고)정규표현식 사용
+				MenuList.printMenu();	// MenuList의 함수 사용, 필요 시 변경
+			}
+			else {
+				if(answer.matches(".*(데리|치킨|새우|불고기)+.*")) {
+					while(true) {
+						if(answer.matches(".*세트.*")) {
+							Order_burger_set(answer);
+							break;
+						}
+						else if(answer.matches(".*단품.*")) {
+							Order_burger_single(answer);
+							break;
+						}
+						else {
+							System.out.println("[키오스크] 세트와 단품 중 어느 것을 주문하시겠습니까?");
+							System.out.print(">>");
+							answer = scanner.nextLine();
+						}
+					}
+					loop_or_not = false;
 				}
 				else {
-					System.out.println("[키오스크] 세트와 단품 중 어느 것을 주문하시겠습니까?");
-					System.out.print(">>");
-					answer = scanner.nextLine();
+					System.out.println("[키오스크] 올바른 메뉴를 선택해주세요.");
+					loop_or_not = true;
 				}
 			}
-		}
-	
+			
+			
+		} while(loop_or_not);
 	}
-	
-	public static void Order_burger_set(String answer) {
-		if(answer.matches(".*데리.*")) {
-			System.out.println("[키오스크] 세트 구성품을 변경하시겠습니까?");
-			if(answer()) {
-				Specific_set("test");
-			}
-			else {
-				Specific_menu("데리버거");
-				Specific_menu("포테이토");
-				Specific_menu("콜라");
-			}
-		}
-		else if(answer.matches(".*치킨.*")) {
-			System.out.println("[키오스크] 세트 구성품을 변경하시겠습니까?");
-			if(answer()) {
-				Specific_set("test");
-			}
-			else {
-				Specific_menu("치킨버거");
-				Specific_menu("포테이토");
-				Specific_menu("콜라");
-			}
-		}
-		else if(answer.matches(".*새우.*")) {
-			System.out.println("[키오스크] 세트 구성품을 변경하시겠습니까?");
-			if(answer()) {
-				Specific_set("test");
-			}
-			else {
-				Specific_menu("새우버거");
-				Specific_menu("포테이토");
-				Specific_menu("콜라");
-			}
-		}
-		else if(answer.matches(".*불고기.*")) {
-			System.out.println("[키오스크] 세트 구성품을 변경하시겠습니까?");
-			if(answer()) {
-				Specific_set("test");
-			}
-			else {
-				Specific_menu("불고기버거");
-				Specific_menu("포테이토");
-				Specific_menu("콜라");
-			}
-		}
-		else {
-			System.out.println("[키오스크] 올바른 메뉴를 선택해주세요.");
-		}
-	}
-	
+		
+	// 버거 단품을 주문 내역에 추가하는 함수
 	public static void Order_burger_single(String answer) {
 		if(answer.matches(".*데리.*")) {
 			Specific_menu("데리버거");
@@ -176,6 +139,73 @@ public class Main {
 			Specific_menu("불고기버거");
 		}
 	}
+	
+	// 버거 세트를 주문 내역에 추가하는 함수(다형성)
+	public static void Order_burger_set(String answer) {
+		if(answer.matches(".*(데리|치킨|새우|불고기)+.*")) {
+			Order_burger_single(answer);
+			System.out.println("[키오스크] 세트 구성품을 변경하시겠습니까?");
+			if(answer()) {
+				Order_burger_set();
+			}
+			else {
+				Specific_menu("포테이토");
+				Specific_menu("콜라");
+			}
+		}
+		else {
+			System.out.println("[키오스크] 올바른 메뉴를 선택해주세요.");
+		}
+	}
+	
+	// 버거 세트 구성품을 변경하여 주문 내역에 추가하는 함수(다형성)
+	public static void Order_burger_set() {	
+		String answer;
+		boolean buffer_desserts = false;
+		boolean buffer_beverages = false;
+		boolean loop_or_not = false;
+		
+		do {
+			System.out.println("[키오스크] 변경하실 메뉴를 선택해주세요.");
+			System.out.print(">>");
+			answer = scanner.nextLine();
+			
+		    for(MenuList.MenuItem item : MenuList.desserts) {
+		        if(answer.equals(item.name) && buffer_desserts == false) {
+		            OrderHistory orderhistory = new OrderHistory(item.name, item.price, item.time, 1);
+		            buffer_desserts = true;
+		        }
+		    }
+		    for(MenuList.MenuItem item : MenuList.beverages) {
+		        if(answer.equals(item.name) && buffer_beverages == false) {
+		            OrderHistory orderhistory = new OrderHistory(item.name, item.price, item.time, 1);
+		            buffer_beverages = true;
+		        }
+		    }
+		    
+		    if(buffer_desserts ^ buffer_beverages) {
+		    	System.out.println("[키오스크] 다른 세트 구성품도 변경하시겠습니까?");
+		    	loop_or_not = answer();
+		    	
+		    	if(loop_or_not == false) {
+		    		if(buffer_desserts) {
+		    			Specific_menu("콜라");
+		    		}
+		    		else if(buffer_beverages) {
+		    			Specific_menu("포테이토");
+		    		}
+		    	}
+		    }
+		    else if(buffer_desserts && buffer_beverages){
+		    	loop_or_not = false;
+		    }
+		    else if(!(buffer_desserts || buffer_beverages)){
+		    	System.out.println("[키오스크] 세트 구성품을 다시 선택해주세요!");
+		    	loop_or_not = true;
+		    }
+		} while(loop_or_not);
+	}
+	
 	
 	// 인자로 전달받은 메뉴 name과 동일한 메뉴를 MenuList에서 찾고, 해당하는 price와 time을 orderhistory에 저장
 	public static void Specific_menu(String name) {
@@ -196,10 +226,6 @@ public class Main {
 	    }
 	}
 	
-	
-	public static void Specific_set(String name) {
-		System.out.println("구현 중");
-	}
 	
 	public static void main(String[] args) {
 	
@@ -225,7 +251,6 @@ public class Main {
 		scanner.close();
 	}
 	
-
 	// 예 아니요 형태의 사용자 입력을 boolean 값으로 반환하는 함수
 	public static boolean answer() {	
 		while(true) {
